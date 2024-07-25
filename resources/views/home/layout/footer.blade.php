c806c55c5ff8fd46f676d370bff1b5a11d9ae704
Murillo Lima <johndoe@murillolimadev.com.br>
2024-07-25 07:53:12 -0300

    modal cadastrar-se anuncie aki

--- resources/views/home/layout/footer.blade.php
+++ resources/views/home/layout/footer.blade.php
@@ -108,8 +108,31 @@
     </footer>
 
     <!-- Back to Top -->
-    <a href="{{ route('home.pages.cadastro.index') }}" class="back-to-top">
+    <a href="" data-toggle="modal" data-target="#modalExemplo" class="back-to-top">
         {{-- divulgar --}}
         <img src="{{ asset('home/images/avatar.png') }}" style="width: 70px" class="divulgar" alt="">
     </a>
     <!-- End footer -->
+    <!-- Modal -->
+    <div class="modal fade" id="modalExemplo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true" style="z-index: 9999999">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Anuncie aqui! Cadastre-se</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form action="" method="post">
+                    <div class="modal-body">
+                      
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                        <button type="button" class="btn btn-primary">Cadastrar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
