be5b65728495282c4a217dd9ec4a2983e82dc706
Murillo Lima <johndoe@murillolimadev.com.br>
2024-07-26 07:17:33 -0300

    controller categoria index admin

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -12,7 +12,8 @@ class CategoriaController extends Controller
      */
     public function index()
     {
-        //
+        $categoria = Categoria::latest()->get();
+        return view('admin.pages.categoria.index', compact('categoria'));
     }
 
     /**
